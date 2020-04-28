16eeb7cc03e4cf23197937c3741957f62200ab54
jhon <jhonjairookawaii@gmail.com>
2020-04-29 00:36:44 +0200

    Add manifest and localStorage (KDA theme);

--- js/app.js
+++ js/app.js
@@ -11,32 +11,52 @@ if (navigator.serviceWorker) {
 }
 
 // KDA
+let active = document.getElementById('switch');
+let fondo = document.body;
+let titulo = document.getElementsByTagName('h1')[0];
+let general = document.getElementsByTagName('a')[0];
+let ajustes = document.getElementsByTagName('a')[1];
+let lugares = document.getElementsByTagName('a')[2];
+let clima = document.getElementsByTagName('a')[3];
+let planes = document.getElementsByTagName('a')[4];
+
 const temaKDA = () => {
-    let active = document.getElementById('switch');
     active.classList.toggle('active');
-
-    let fondo = document.body;
     fondo.classList.toggle('fondo--kda');
-
-    let titulo = document.getElementsByTagName('h1')[0];
     titulo.classList.toggle('kda');
-
-    // CUIDADO CON EL ORDEN - si se modifica el index y se añaden <a>
-    let general = document.getElementsByTagName('a')[0];
     general.classList.toggle('panel__boton--general_kda');
-
-    let ajustes = document.getElementsByTagName('a')[1];
     ajustes.classList.toggle('panel__boton--ajustes_kda');
-
-    let lugares = document.getElementsByTagName('a')[2];
     lugares.classList.toggle('panel__boton--ajustes_kda');
-
-    let clima = document.getElementsByTagName('a')[3];
     clima.classList.toggle('panel__boton--ajustes_kda');
-
-    let planes = document.getElementsByTagName('a')[4];
     planes.classList.toggle('panel__boton--ajustes_kda');
+
+    // Guardar modo
+    if (document.body.classList.contains('fondo--kda')) {
+        localStorage.setItem('kda-mode', 'true');
+    } else {
+        localStorage.setItem('kda-mode', 'false');
+    }
+}
+
+// Obtener modo
+if (localStorage.getItem('kda-mode') === 'true') {
+    active.classList.add('active');
+    fondo.classList.add('fondo--kda');
+    titulo.classList.add('kda');
+    general.classList.add('panel__boton--general_kda');
+    ajustes.classList.add('panel__boton--ajustes_kda');
+    lugares.classList.add('panel__boton--ajustes_kda');
+    clima.classList.add('panel__boton--ajustes_kda');
+    planes.classList.add('panel__boton--ajustes_kda');
+} else {
+    active.classList.remove('active');
+    fondo.classList.remove('fondo--kda');
+    titulo.classList.remove('kda');
+    general.classList.remove('panel__boton--general_kda');
+    ajustes.classList.remove('panel__boton--ajustes_kda');
+    lugares.classList.remove('panel__boton--ajustes_kda');
+    clima.classList.remove('panel__boton--ajustes_kda');
+    planes.classList.remove('panel__boton--ajustes_kda');
 }
 
 document.getElementById('switch').addEventListener('click', temaKDA, false);
-
